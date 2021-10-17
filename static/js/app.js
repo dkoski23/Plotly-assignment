4c24779716168adb61859cd19ca42d2f2525838e
unknown <dkoski23@gmail.com>
2021-10-16 17:12:54 -0700

    reading in with d3 and getting all ids to the dropdown button

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,15 @@
 function testing(){
-    alert("Testing");
-}
+        var selector = d3.select("#selDataset");
+        console.log(selector)
+        d3.json("samples.json").then(function(data) {
+            console.log(data);
+            var names = data.names;
+            names.forEach(function(name) {
+                selector
+                .append("option")
+                .text(name)
+                .property("value", name)
+            })        
+        })
+};
 testing()
