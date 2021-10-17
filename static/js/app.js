8c9b4e51e8506e11a333e1cb2baec02e27c15fc1
unknown <dkoski23@gmail.com>
2021-10-16 17:30:29 -0700

    made a function to collect all demographic information using d3

--- static/js/app.js
+++ static/js/app.js
@@ -1,15 +1,34 @@
-function testing(){
+function getIDs(){
         var selector = d3.select("#selDataset");
-        console.log(selector)
+        // console.log(selector)
         d3.json("samples.json").then(function(data) {
-            console.log(data);
-            var names = data.names;
-            names.forEach(function(name) {
+            // console.log(data);
+            var ids = data.names;
+            ids.forEach(function(id) {
                 selector
                 .append("option")
-                .text(name)
-                .property("value", name)
-            })        
+                .text(id)
+                .property("value", id)
+            })   
+            // console.log(selector)
+            console.log(ids)     
         })
 };
-testing()
+getIDs()
+
+function getDemographics(){
+    var selector = d3.select("#sample-metadata");
+    // console.log(selector)
+    d3.json("samples.json").then(function(metadata) {
+        // console.log(metadata);
+         var people = metadata.metadata;
+        // people.forEach(function(person) {
+        //     selector
+        //     .append("option")
+        //     .text(person)
+        //     .property("value", person)
+        // })   
+        console.log(people[0])     
+    })
+};
+getDemographics()
